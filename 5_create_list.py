c7136eb4b0411ef1fe95efd3bc5a65cda06c9201
fourmi <44077202+fourmi1995@users.noreply.github.com>
2018-11-11 21:30:57 +0800

    Update 5_create_list.py

--- 5_create_list.py
+++ 5_create_list.py
@@ -6,12 +6,12 @@ testPath = '/home/fourmi/桌面/IronData_Camvid/testing/imgs/'
 for root, dirs, files in os.walk(trainPath):
     for file in files:
         f = open('train.txt','a')
-        f.write('./IronData_Camvid/training/imgs/'+file+' '+'./IronData_Camvid/training/labels/'+file+'\n')
+        f.write('./IronData_Camvid/training/imgs/'+file+' '+'./IronData_Camvid/training/labels/'+file[:-4]+'.png\n')
         f.close()
 
 for root, dirs, files in os.walk(testPath):
     for file in files:
         f = open('test.txt','a')
-        f.write('./IronData_Camvid/testing/imgs/'+file+' '+'./IronData_Camvid/testing/labels/'+file+'\n')
+        f.write('./IronData_Camvid/testing/imgs/'+file+' '+'./IronData_Camvid/testing/labels/'+file[:-4]+'.png\n')
         f.close()
 
